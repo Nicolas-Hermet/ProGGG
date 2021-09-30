6df43495a0580799ed59b80718bdfba3b3acc694
NicoH <n.hermet@gmail.com>
2021-09-30 16:18:50 +0200

    import from stackblitz

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,49 +1,67 @@
-import { PlaylistService } from './playlist.service'
-import { Component } from '@angular/core'
-import { Subject, Subscription } from 'rxjs'
+import { PlaylistService } from './playlist.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { fromEvent, interval, Subject, Subscription } from 'rxjs';
+import { debounceTime, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ProGGG-Angular'
-  tracks: string[] = []
-  toto: any
-  absolute: any
-  alpha: any
-  beta: any
-  gamma: any
+export class AppComponent implements OnInit, OnDestroy {
+  title = 'ProGGG-Angular';
+  tracks: string[] = [];
+  absolute: any;
+  alpha: any;
+  beta: any;
+  gamma: any;
 
-  triggerSubject = new Subject()
-  trackSubscription: Subscription | undefined
+  triggerSubject = new Subject();
+  private _destroyed$ = new Subject();
+  trackSubscription: Subscription | undefined;
+  permissionGranted: boolean = false;
 
-  constructor(private playlistService: PlaylistService) {}
+  constructor(private playlistService: PlaylistService) {
+    window.addEventListener('deviceorientation', event => {
+      if (event.absolute == null) {
+        DeviceOrientationEvent.requestPermission()
+          .then((permissionState: any) => {
+            if (permissionState === 'granted') {
+              this.permissionGranted = true;
+            }
+          })
+          .catch(console.error);
+      }
+    });
+  }
 
   ngOnInit() {
-    // window.addEventListener('deviceorientation', event => {
-    //   this.absolute = event.absolute
-    //   this.alpha = event.alpha
-    //   this.beta = event.beta
-    //   this.gamma = event.gamma
-    //   if (Math.abs(this.gamma) < 40) {
-    //     this.triggerSubject.next(this.playlistService.getRandomTrack())
-    //   }
-    // })
-    // window.addEventListener('devicemotion', e => {
-    //   if (e.acceleration) {
-    //     this.toto = e.acceleration.z
-    //   }
-    // })
-    setInterval(() => {
-      this.triggerSubject.next(this.playlistService.getRandomTrack())
-    }, 2000);
-
-    this.trackSubscription = this.triggerSubject.subscribe(
-      (track: any) => {
-        this.tracks.push(track)
+    window.addEventListener('deviceorientation', event => {
+      this.absolute = event.absolute;
+      this.alpha = event.alpha;
+      this.beta = event.beta;
+      this.gamma = event.gamma;
+      if (Math.abs(this.gamma) < 40) {
+        console.trace(
+          '%c Oh my heavens! ',
+          'background: #222; color: #bada55',
+          this.gamma
+        );
+        this.triggerSubject.next(this.playlistService.getRandomNumber());
       }
-    );
+    });
+
+    this.trackSubscription = this.triggerSubject
+      .pipe(
+        debounceTime(200),
+        takeUntil(this._destroyed$)
+      )
+      .subscribe((trackNumber: any) => {
+        this.tracks.push(this.playlistService.getTrack(trackNumber));
+      });
+  }
+
+  ngOnDestroy(): void {
+    this._destroyed$.complete();
   }
 }
--- src/app/playlist.service.ts
+++ src/app/playlist.service.ts
@@ -1,6 +1,5 @@
-import { Injectable } from '@angular/core'
+import { Injectable } from '@angular/core';
 import playlists from '../assets/playlist.json';
-
 
 @Injectable({
   providedIn: 'root'
@@ -8,12 +7,21 @@ import playlists from '../assets/playlist.json';
 export class PlaylistService {
   private tracks = playlists.tracks;
 
-  constructor() { }
+  constructor() {}
 
-  getRandomTrack(){
-    let playlistLength = this.tracks.length
-    let trackNumber = Math.floor(Math.random() * playlistLength)
-    console.log(trackNumber)
-    return this.tracks[trackNumber]
+  getRandomTrack() {
+    let playlistLength = this.tracks.length;
+    let trackNumber = Math.floor(Math.random() * playlistLength);
+    console.log(trackNumber);
+    return this.tracks[trackNumber];
+  }
+
+  getRandomNumber() {
+    let playlistLength = this.tracks.length;
+    return Math.floor(Math.random() * playlistLength);
+  }
+
+  getTrack(num: number) {
+    return this.tracks.splice(num, 1)[0];
   }
 }
